3779d4de074007f5762cdf6812e073c78f473f40
ak0501 <avinashkhanal@gmail.com>
2020-06-22 21:25:52 -0500

    updated symantics

--- script.js
+++ script.js
@@ -31,13 +31,21 @@ var storeItems = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4
 // when the page refreshes the events are saved
 // 
 
+//var myStorage = window.localStorage
+
+
 // when text is entered in the text area and clicked saved ; the data is saved and stored 
 // need a function retrieve the data from the local storage to the page
 function retrieve() {
-    var saveData = JSON.parse(localStorage.getItem("schedule", [])) || [];
+    //TO PUT HTINGS INTO LOCAL STORAGE
+    //JSON.stringify() --> takes an object, number, function, array, string, boolean turns
+    // it into a string
+
+    //TO GET THINGS OUT OF LOCAL STORAGE
+    //JSON.parse()---> takes a string, and returns the original data type
+    var saveData = JSON.parse(window.localStorage.getItem("schedule", [])) || [];
     for (let i = 0; i < storeItems.length; i++) {
         $(storeItems[i]).text(saveData[i]);
-
     }
 
 };
@@ -49,47 +57,47 @@ function save() {
 }
 
 $("#btn9").on("click", function () {
-    hrNote[0] = $("#9am").val();
-    console.log(hrNote)
+    hrNote[0] = $("#9amButton").val();
+    console.log('HRNOTE-->', hrNote)
     save();
 });
 $("#btn10").on("click", function () {
-    hrNote[1] = $("#10am").val();
+    hrNote[1] = $("#10amButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn11").on("click", function () {
-    hrNote[2] = $("#11am").val();
+    hrNote[2] = $("#11amButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn12").on("click", function () {
-    hrNote[3] = $("#12am").val();
+    hrNote[3] = $("#12pmButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn1").on("click", function () {
-    hrNote[4] = $("#1pm").val();
+    hrNote[4] = $("#1pmButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn2").on("click", function () {
-    hrNote[1] = $("#2pm").val();
+    hrNote[5] = $("#2pmButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn3").on("click", function () {
-    hrNote[1] = $("#3pm").val();
+    hrNote[6] = $("#3pmButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn4").on("click", function () {
-    hrNote[1] = $("#4pm").val();
+    hrNote[7] = $("#4pmButton").val();
     console.log(hrNote)
     save();
 });
 $("#btn5").on("click", function () {
-    hrNote[1] = $("#5pm").val();
+    hrNote[8] = $("#5pmButton").val();
     console.log(hrNote)
     save();
 });
@@ -99,14 +107,15 @@ var x = 0;
 var timeSlots = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 for (let x = 0; x < hrNote.length; x++) {
-
     if (hour === timeSlots[x]) {
         $(storeItems[x]).attr("class", "present")
     }
     if (hour < timeSlots[x]) {
         $(storeItems[x]).attr("class", "past")
-    }
-    else {
+    } else {
         $(storeItems[x]).attr("class", "future")
     }
-};
+}
+// console.log(hour)
+// console.log(timeSlots[x])
+// console.log((moment().hour()
